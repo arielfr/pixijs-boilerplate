07b8ca63343210ae526b27414d33d44fa6ae4945
Ariel Rey <ariel.rey@mercadolibre.com>
2020-04-10 15:54:26 -0300

    Refactoring webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,24 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 const isProduction = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-  entry: './game/index.js',
+  entry: {
+    'game': './game/index.js',
+    'pixi': './resources/pixi.js'
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Game Demo',
+      title: 'Game',
       template: './templates/index.html',
       production: isProduction,
       inject: false,
     }),
-    new CopyPlugin([
-      { from: 'public', to: 'dest' },
-    ]),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'game.js'
   },
 };

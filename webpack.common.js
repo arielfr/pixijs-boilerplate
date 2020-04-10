ad704f5454efe0c29053bb82416e8550053181c7
Ariel Rey <ariel.rey@mercadolibre.com>
2020-04-10 14:19:16 -0300

    Base Webpack Configuration

---
+++ webpack.common.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './game/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'tetriscript.js'
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        test: /\.js(\?.*)?$/i,
+      }),
+    ],
+  },
+});

c0e135f69f5b2c6ddc28ff433d540ca22e557b4e
Ariel Rey <ariel.rey@mercadolibre.com>
2020-04-11 14:20:33 -0300

    Adding babel

--- webpack.common.js
+++ webpack.common.js
@@ -27,6 +27,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test:/\.(js|jsx)$/,
+        exclude:/node_modules/,
+        use:['babel-loader']
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
